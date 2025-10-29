0c6e7d5bb7a680d41a2d75f3714f7163a0a13e8f
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2025-10-29 10:26:48 +0200

    Add files via upload

---
+++ Τσιούβα/Προγραμματισμός/1ο.cpp
@@ -0,0 +1,15 @@
+//justpaste.it/amrz6
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+    system ("CHCP 1253 > nul"); //για τα Ελληνικά
+
+   printf("Kαλημέρα!\n");
+   
+   system("PAUSE");
+
+	return 0;
+}
