1b1ef05811c6c833ac09ad4b7a428f328032d09a
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2025-11-05 04:26:20 +0200

    Add files via upload

---
+++ Τσιούβα/Προγραμματισμός/scanf.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	system("CHCP 65001 > nul");
+	
+	int number;
+ 
+ 	printf("Ti timh thes na exei;");
+ 	scanf("%d", &number);
+ 	
+ 	printf("H metavlhth exei timh %d. \n", number);
+	system ("PAUSE");
+	return 0;
+}
