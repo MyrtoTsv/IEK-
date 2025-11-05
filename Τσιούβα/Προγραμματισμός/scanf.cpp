2aeae2ef691db513d886d93e190b89e792467d98
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2025-11-05 10:21:03 +0200

    pisina.cpp

--- Τσιούβα/Προγραμματισμός/scanf.cpp
+++ Τσιούβα/Προγραμματισμός/scanf.cpp
@@ -2,14 +2,24 @@
 #include <stdlib.h>
 
 int main(){
-	system("CHCP 65001 > nul");
+	system("CHCP 1253 > nul");
 	
-	int number;
+	float mhkos;
+	float platos;
+	float ipsos;
+	float emvadon,ogkos;
+   
+	//ôï number å÷åé áðïèçêåõôåé óôçí ìíçìç ñáì
  
- 	printf("Ti timh thes na exei;");
- 	scanf("%d", &number);
+ 	printf("Äþóå ìïõ ôï ìÞêïò , ôï ðëÜôïò êéá ôï ýøïò ðïõ èá Þèåëåò íá Ý÷åé ç ðéóßíá \n");
+ 	scanf("%f %f %f", &mhkos , &platos ,&ipsos); //óôçí äéåèõíóç ðïõ õðáñ÷åé óôçí ñáì
  	
- 	printf("H metavlhth exei timh %d. \n", number);
+ 	emvadon = platos*mhkos;
+ 	ogkos = mhkos*ipsos*platos;
+ 	
+ 	printf("Ôá ôåôñáãùíéêá ìåôñá ìå ðëáêáêé ðïõ ðñåðåé íá ìðïõí óôïí ðõèìåíá ôçò ðéóéíáò åéíáé: %.2lf ôåôñáãùíéêá ìåôñá\n",emvadon);
+ 	printf("Ï üãêïò ôïõ íåñïý ðïõ ÷ùñÜåé ïëüêëçñç ç ðéóßíá åßíáé: %.2lf êõâéêá ìåôñá\n",ogkos);
+ 	
 	system ("PAUSE");
 	return 0;
 }
