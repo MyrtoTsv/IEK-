4b02106beffded24cd8631b0b19658a89dcfbdd8
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2025-11-25 21:48:57 +0200

    Add files via upload

---
+++ 1ο εξάμηνο/Προγραμματισμός/MonosZugos.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main (){
+   float num;
+ 	
+   printf("Dwse mou enan akeraio\n");
+   scanf("%f",&num);
+ 	
+   if (num!=(int)num)
+     printf("Den einai akeraios\n");
+   else 
+     if ((int)num%2==0)
+       printf("O arithmos einai zugos");
+     else if ((int)num%2!=0)
+       printf("O arithmos einai monos");
+
+   printf("\n");
+   system("PAUSE > nul");
+   return 0;
+ }
