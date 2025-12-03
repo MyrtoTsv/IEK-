5024be6f8a6a49b46f83e9a479e545c8938b0c45
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2025-12-03 09:26:04 +0200

    Update Kompiouteraki.cpp

--- 1ο εξάμηνο/Προγραμματισμός/Homework/Kompiouteraki.cpp
+++ 1ο εξάμηνο/Προγραμματισμός/Homework/Kompiouteraki.cpp
@@ -1,46 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h> //Gia ta true false bool
 
 int main(){
 	
 	float num1,num2,result;
-	char praksh;
+	char praksh;//valid values are +,-,*,/
+	bool flag = true;//true means everything is ok
 	
 	printf("Dwse mou enan arithmo\n");
-	scanf(" %f",&num1);
+	scanf("%f",&num1);
 	
 	printf("Dwse mou ena symvolo prakshs\n");
-	scanf(" %c",&praksh);
+	scanf("%c",&praksh);
+	getchar();//empty buffer from ENTER
 	
 	printf("Dwse mou enan deutero arithmo\n");
-	scanf(" %f",&num2);
+	scanf("%f",&num2);
 	
-	if (praksh=='+') {
+	if (praksh=='+') 
         result=num1+num2;
-        printf("To apotelesma einai: %.2f\n",result);
-    }
-    else if (praksh=='-') {
+    else if (praksh=='-') 
         result=num1-num2;
-        printf("To apotelesma einai: %.2f\n",result);
-    }
-    else if (praksh=='*') {
+    else if (praksh=='*') 
         result=num1*num2;
-        printf("To apotelesma einai: %.2f\n",result);
-    }
-    else if (praksh=='/') {
+    else if (praksh=='/') 
         if (num2==0) {
-            printf("Diareis me to mhden. Lathos!\n");
-        } else {
+		
             result =num1/num2;
-            printf("To apotelesma einai: %.2f\n",result);
-        }
-    }
-    else {
-        printf("Lathos symbolo prakshs!\n");
-    }
-
+        }else{
+		
+            flag = false;
+            printf("Diaireis me to 0!\n");}
+    else{
+	
+        flag = false;
+    }    printf("Lathos symbolo prakshs!\n");
+    
+	if(flag==true)    
+      printf("To apotelesma einai: %.2f\n",result);
+    
 	
 	printf("\n");
-	system("PAUSE > nul");
+	system("PAUSE");
 	return 0;
 }
