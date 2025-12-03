e3055e014c856992892e49f58a524a3d5b9fd431
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2025-12-03 11:18:57 +0200

    Add files via upload

---
+++ 1ο εξάμηνο/Προγραμματισμός/Codes/While.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	
+	int i,sum;
+	sum=0;
+	for(i=0; i<=36; i++){
+		sum = sum + i;
+	}
+ 
+ printf("%d\n",sum);
+ system("PAUSE");
+ return 0;
+}
