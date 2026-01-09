5afa805a4ed272155ec351fb55bad74909a8a6ce
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2026-01-09 12:37:04 +0200

    Create aseembly+1.asm

---
+++ 1ο εξάμηνο/Αρχιτεκτονική Υπολογιστών/aseembly+1.asm
@@ -0,0 +1,17 @@
+.data #etsi ksekiname,tmhma dedomenwn
+.text #thma kwdika
+
+main: #edw grafoume ton kwdika , mesa sth main
+   	
+   	#fortwnw oti exei h dieuthnsh mnhmhs 
+	lw $t0 ,0x10010000
+	lw $t1 0x10010020
+	#prosthesh ston kataxwrhth t2 , t2=t1+t0
+	add $t2 ,$t1 , $t0
+	#apothikeuoume to apotelesma sthn mnhmh RAM
+	#theloume twra t2=t2 +1.
+	li $t3 ,1
+	add $t2,$t2,$t3
+	sw $t2 0x10010040
+	li $v0 , 10 #klhsh eksodou(call code for exit=10)
+	syscall #call operating sys
