7a66e6c3b1cfc2a6bec947f7748907cc0c49ed3a
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2026-01-09 12:43:11 +0200

    Update 1.1aseembly.asm

--- 1ο εξάμηνο/Αρχιτεκτονική Υπολογιστών/1.1aseembly.asm
+++ 1ο εξάμηνο/Αρχιτεκτονική Υπολογιστών/1.1aseembly.asm
@@ -15,3 +15,16 @@ main: #edw grafoume ton kwdika , mesa sth main
 	sw $t2 0x10010040
 	li $v0 , 10 #klhsh eksodou(call code for exit=10)
 	syscall #call operating sys
+
+#h mia pio grhgorh lush 
+	#fortwnw oti exei h dieuthnsh mnhmhs 
+	lw $t0 ,0x10010000
+	lw $t1 0x10010020
+	#prosthesh ston kataxwrhth t2 , t2=t1+t0
+	add $t2 ,$t1 , $t0
+	#apothikeuoume to apotelesma sthn mnhmh RAM
+	#theloume twra t2=t2 +1.
+	#li $t3 ,1
+	addi $t2,$t2,1
+	sw $t2 0x10010040 
+me thn addi
