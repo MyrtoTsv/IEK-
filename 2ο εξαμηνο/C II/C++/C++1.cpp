eb553400f72633a720c2b2cc685a8c5f32d66e8b
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2026-04-21 09:22:55 +0300

    Add files via upload

---
+++ 2ο εξαμηνο/C II/C++/C++1.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //input output stream.Anti gia library tha einai stream
+#include <cstdlib> //xrhsimopoioume to library ths c , kai vasoume ena c mprosta
+
+int main(){
+	
+	
+	system("PAUSE");
+	return 0;
+}
