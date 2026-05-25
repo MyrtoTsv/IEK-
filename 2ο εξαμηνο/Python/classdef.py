244837d1a1404cc75bf73ce09b2b58366ff822aa
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2026-05-25 10:29:18 +0300

    Create classdef.py

---
+++ 2ο εξαμηνο/Python/classdef.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self,balance):
+        self.balance = balance
+#Apofeugoume ta munhmata stis sunarthseis ths klashs, den theloume na epikoinwnoume me ton
+#xrthsth mesa sthm klash , mono mesa sthn main , giati prostateoume ta pedia ths klashs
+    def deposit(self,amount):
+        if(amount < 0):
+            self.balance+=amount
+
+    def show_balance(self):
+        print(self.balance)
+
+
+acc1 = BankAccount(360)
+acc1.deposit(-500)
