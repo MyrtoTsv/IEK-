f8543a3a48923495240e4cc6201a83aa2c041f85
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2026-05-25 11:04:05 +0300

    Update classdef.py

--- 2ο εξαμηνο/Python/classdef.py
+++ 2ο εξαμηνο/Python/classdef.py
@@ -13,3 +13,23 @@ class BankAccount:
 
 acc1 = BankAccount(360)
 acc1.deposit(-500)
+
+class BankAccount:
+    def __init__(self,balance):
+        self.balance = balance
+
+    def deposit(self,amount):
+        if(amount > 0):
+            self.balance+=amount
+
+    def show_balance(self):
+        print(self.balance)
+
+    def __str__(self):
+        return "Ypoloipo:" + str(self.balance) + " " + "euro"
+
+
+
+acc1 = BankAccount(360)
+acc1.deposit(500)
+print(acc1)
