875399a9a7d74e99e7efbdab6c801548619fdfa8
MyrtoTsv <70771993+MyrtoTsv@users.noreply.github.com>
2026-05-26 11:25:26 +0300

    Create Vehicle.cpp

---
+++ 2ο εξαμηνο/C II/C++/Askhsh-006/Vehicle.cpp
@@ -0,0 +1,89 @@
+#include "Vehicle.h"
+
+int Vehicle::count=0;
+
+int Vehicle::getCount()
+{
+    return count;
+}
+//custom constructor
+Vehicle::Vehicle(string id,int speed,int attack,int defence)
+{
+	setAll(id,speed,attack,defence);
+  count++;
+	cout<<"Object "<<id<<" "<<speed<<" "<<attack<<" "<<" "<<defence
+	<<" was created"<<endl;
+}
+
+//destructor
+~Vehicle::Vehicle()
+{
+	
+}
+
+//h setAll exei olous tous setters giati kanei set se ola
+void Vehicle::setAll(string id,int speed,int attack,int defence)
+{
+	setId(id);
+	setSpeed(speed);
+	setAttack(attack);
+	setDefense(defence);
+}
+
+void Vehicle::setId(string id)
+{
+	this->id = id;
+}
+
+void Vehicle::setSpeed(int speed)
+{
+	this->speed = speed;
+		
+}
+
+void Vehicle::setAttack(int attack)
+{
+	this->attack=attack;
+}
+
+void Vehicle::setDefence(int defence)
+{
+	this->defence=defence;
+}
+
+static int Vehicle::getCount() const
+{
+	this->count=count;
+}
+
+
+string Vehicle::getId() const
+{
+	return id;
+}
+
+int Vehicle::getSpeed() const
+{
+	return speed;
+}
+
+int Vehicle::getAttack() const
+{
+	return attack;
+}
+
+int Vehicle::getDefence() const
+{
+	return defence;
+}
+
+//h print exei olous tous getters gt tupwnei ta apotelesmata
+void Vehicle::print() const
+{
+	cout<<getCount()<<endl;
+	cout<<getId()<<endl;
+	cout<<getSpeed()<<endl;
+	cout<<getAttack()<<endl;
+	cout<<getDefence()<<endl;
+	
+}
